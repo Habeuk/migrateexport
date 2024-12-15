9cc91b26f03a4ae8ab915558a1fd22d7ed69e626
kouwa stephane <kksasteph888@gmail.com>
2024-12-15 11:48:58 +0100

    Maj

--- src/Services/ManageEntities.php
+++ src/Services/ManageEntities.php
@@ -13,14 +13,14 @@ use Drupal\migrateexport\Services\Exception\ExceptionMigrate;
  */
 class ManageEntities {
   protected $entity_get_info = [];
-
+  
   /**
    * Contient toutes les informations sur la definition des champs.
    *
    * @var array
    */
   protected $infosAboutFiels = [];
-
+  
   /**
    * il faudra tester cette fonction et voir ce quelle renvoie vs
    * loadFullDefinitionOfentity.
@@ -49,7 +49,7 @@ class ManageEntities {
     // $this->debug($entities, 'loadEntities', true);
     return $entities;
   }
-
+  
   /**
    * Retourne toutes les definitions d'entites.
    *
@@ -58,9 +58,10 @@ class ManageEntities {
   public function LoadAllentities() {
     if (!$this->entity_get_info)
       $this->entity_get_info = entity_get_info();
+    
     return $this->entity_get_info;
   }
-
+  
   /**
    * Recupere toutes la definition de l'entité.
    *
@@ -86,7 +87,7 @@ class ManageEntities {
     }
     return [];
   }
-
+  
   public function loadFullDefinitionOfentityAndbundle(string $entity_type_id, $bundle) {
     $entity_get_info = $this->LoadAllentities();
     if (!empty($entity_get_info[$entity_type_id])) {
@@ -94,7 +95,19 @@ class ManageEntities {
         $bundle => $bundle
       ];
       $bundle_keys = $entity_get_info[$entity_type_id]['bundle keys'];
+      /**
+       * Entité de configuration portant les bundles.
+       * example : 'node_type'
+       *
+       * @var string $entity_base_type
+       */
       $entity_base_type = null;
+      /**
+       * Colonne de la bd portant le bundle
+       * example: 'type'
+       *
+       * @var string $column_bundle_id
+       */
       $column_bundle_id = null;
       if (!empty($bundle_keys['bundle'])) {
         $entity_base_type = $entity_type_id . '_' . $bundle_keys['bundle'];
@@ -104,7 +117,7 @@ class ManageEntities {
     }
     return [];
   }
-
+  
   /**
    * //
    *
@@ -115,22 +128,42 @@ class ManageEntities {
    */
   protected function loadResumeEntityType($entity_type_id = 'node', $bundles = [], $entity_base_type = null, $column_bundle_id = null) {
     $results = [];
+    
     if ($bundles) {
       foreach ($bundles as $bundle => $label) {
         $query = new \EntityFieldQuery();
-        $query->entityCondition('entity_type', $entity_type_id, '=')->propertyCondition('type', $bundle, '=');
+        $query->entityCondition('entity_type', $entity_type_id, '=');
+        
+        if ('taxonomy_term' == $entity_type_id) {
+          $query_select = db_select('taxonomy_vocabulary', "vb");
+          $query_select->fields("vb", [
+            'machine_name',
+            'vid'
+          ]);
+          $query_select->condition('machine_name', $bundle);
+          $result_select = $query_select->execute()->fetchAssoc();
+          if (isset($result_select['vid'])) {
+            $query->propertyCondition('vid', $result_select['vid'], '=');
+          }
+        }
+        else
+          $query->propertyCondition($column_bundle_id, $bundle, '=');
         $result = $query->execute();
         $result = $query->count()->execute();
+        
         $results[$bundle] = [
           'label' => $label,
           'count_entities' => $result, // nom de contenu
           'fields' => $this->filterField($entity_type_id, $bundle),
           'extra_fields' => $this->getBundleExtraFields($entity_type_id, $bundle)
         ];
-        if ($entity_base_type)
+        if ($entity_base_type && !in_array($entity_type_id, [
+          'taxonomy_term'
+        ]))
           $results[$bundle]['content'] = $this->getEntityTypeData($bundle, $entity_base_type, $column_bundle_id);
       }
-    } else {
+    }
+    else {
       $bundle = $entity_type_id;
       $query = new \EntityFieldQuery();
       $query->entityCondition('entity_type', $entity_type_id, '=');
@@ -144,18 +177,18 @@ class ManageEntities {
     }
     return $results;
   }
-
+  
   protected function getEntityTypeData($bundle, $entity_base_type, $column_bundle_id) {
     $query = db_select($entity_base_type, 'nt')->fields('nt')->condition($column_bundle_id, $bundle);
     $result = $query->execute();
     return $result->fetchAssoc();
   }
-
+  
   protected function getBundleExtraFields($entity_type_id, $bundle) {
     $FieldInfo = _field_info_field_cache();
     return $FieldInfo->getBundleExtraFields($entity_type_id, $bundle);
   }
-
+  
   /**
    *
    * @param string $entity_type_id
@@ -174,7 +207,7 @@ class ManageEntities {
     }
     return $fields;
   }
-
+  
   protected function getInfoAboutTypeOfField() {
     if (!$this->infosAboutFiels) {
       $this->infosAboutFiels = _field_info_collate_types();
